078750d42ec96749dc934cd29370c255faede6bd
Tobias Ericsson <tobias.ericsson@mobenga.com>
2018-08-29 11:23:38 +0200

    spring.io

---
+++ spring.io/README.md
@@ -0,0 +1,19 @@
+
+mvn spring-boot:run
+
+
+
+Actuator Endpoints:
+
+* /check  (all actuator endpoints)
+
+### example endpoints:
+
+* /check/info
+* /check/health
+* /check/metrics/system.cpu.usage
+
+
+mysql:
+
+mysql -h 127.0.0.1 -P 3306 -u root -p 
---
+++ spring.io/endpoint/src/main/java/com/toer/endpoint/health/MyMySQLHealthIndicator.java
@@ -0,0 +1,34 @@
+package com.toer.endpoint.health;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class MyMySQLHealthIndicator implements HealthIndicator {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private static final String SQL = "select * from test";
+
+    @Override
+    public Health health() {
+        int errorCode = 0; // perform some specific health check
+        try {
+            List<Map<String, Object>> rows = jdbcTemplate.queryForList(SQL);
+        } catch (Exception e) {
+            errorCode = -1;
+        }
+
+        if (errorCode != 0) {
+            return Health.down().withDetail("Error Code", errorCode).build();
+        }
+        return Health.up().build();
+    }
+}
