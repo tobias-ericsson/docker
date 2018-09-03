288dc67ce1d0676be12d908beefcdaadc98d8d2a
Tobias Ericsson <t.ericsson@gmail.com>
2018-09-03 11:37:13 +0200

    receiver producer

--- spring.io/producer/src/main/java/com/toer/producer/web/WebController.java
+++ spring.io/producer/src/main/java/com/toer/producer/web/WebController.java
@@ -7,16 +7,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
-@RequestMapping(value = "/send")
+@RequestMapping(value = "/")
 public class WebController {
 
     @Autowired
     Sender sender;
 
     @RequestMapping(method=GET)
+    public String root() {
+
+        String pre = "<p><a href='";
+        String post = "</p>";
+
+        List<String> urls = new ArrayList<String>();
+        urls.add("/send?message=test");
+        urls.add("http://localhost:15672");
+
+        String result = urls.stream().
+                map(url -> pre+url+"'/>"+url+post).
+                reduce("", (a,b) -> a + b);
+
+        return "<body>"+result+"</body>";
+    }
+
+    @RequestMapping(method=GET, value = "send")
     public String producer(@RequestParam("message") String message) {
 
         sender.sendMessage(message);
