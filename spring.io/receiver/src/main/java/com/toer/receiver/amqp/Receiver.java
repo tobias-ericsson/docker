27084d5c8e8805ed90e70cbd802d3532811b1691
Tobias Ericsson <t.ericsson@gmail.com>
2018-09-06 11:22:45 +0200

    cassandra

--- spring.io/receiver/src/main/java/com/toer/receiver/amqp/Receiver.java
+++ spring.io/receiver/src/main/java/com/toer/receiver/amqp/Receiver.java
@@ -1,17 +1,33 @@
 package com.toer.receiver.amqp;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 import java.util.concurrent.CountDownLatch;
+
+import com.toer.receiver.db.Message;
+import com.toer.receiver.db.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Receiver {
 
     private CountDownLatch latch = new CountDownLatch(1);
+    @Autowired
+    MessageRepository repository;
 
-    public void receiveMessage(String message) {
-        System.out.println("Received <" + message + ">");
+    public void receiveMessage(String mess) {
+        System.out.println("Received <" + mess + ">");
         latch.countDown();
+
+        Message message = new Message();
+        message.setId(System.currentTimeMillis());
+        message.setTitle(mess);
+        message.setBody(mess);
+        message.setDate(new Date().toString());
+
+        repository.save(message);
+
     }
 
     public void receiveMessage(byte[] message) {
