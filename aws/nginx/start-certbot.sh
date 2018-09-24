b4f879ae182dadcaaeaa741dffa741a2a69c1ded
Tobias Ericsson <t.ericsson@gmail.com>
2018-09-24 22:33:01 +0200

    nginx certbot

---
+++ aws/nginx/start-certbot.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+sudo docker run -it --rm \
+-v $(pwd)/letsencrypt:/etc/letsencrypt \
+-v $(pwd)/html:/data/letsencrypt \
+certbot/certbot \
+certonly --webroot \
+--email t.ericsson@gmail.com --agree-tos --no-eff-email \
+--webroot-path=/data/letsencrypt \
+-d tibbes.com -d www.tibbes.com
+
+#--register-unsafely-without-email --agree-tos \
+#--webroot-path=/data/letsencrypt \
+#--staging \
+#-d tibbes.com -d www.tibbes.com
+#-d week.tibbes.com -d veckogott.tibbes.com -d seawind.tibbes.com
